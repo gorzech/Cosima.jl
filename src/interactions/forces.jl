745044fcffa24904df2d2a0fc85802232b8013c2
Grzegorz Orzechowski <2590699+gorzech@users.noreply.github.com>
2023-08-10 23:59:41 +0300

    feat: add forces

---
+++ src/interactions/forces.jl
@@ -0,0 +1,27 @@
+abstract type Force end
+
+struct ForceTorque{T<:Body} <: Force
+    body::T
+    psi::Matrix{Float64}
+    torque::Function
+end
+
+function ForceTorque(body::RBody, torque::Function)
+    @assert size(torque(0.0)) == (3,)
+    ForceTorque(body, zeros(0, 0), torque)
+end
+
+function force!(Q, t, f::ForceTorque{RBody})
+    T = f.torque(t)
+    pp_i = f.body.q[4:7]
+    A_i = rot(pp_i)
+    T_i = A_i' * T 
+
+    Q[f.body.hi[4:end]] .+= T_i
+end
+
+function force(t, f::ForceTorque{<:Body})
+    Q = empty(f.body.nh)
+    force!(Q, t, f)
+    return Q
+end
